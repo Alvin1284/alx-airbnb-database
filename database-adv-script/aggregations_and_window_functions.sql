15208c8d77809dd69687dc9217bedf707a0939bd
Alvin Waweru <81688085+Alvin1284@users.noreply.github.com>
2025-05-16 23:27:54 +0300

    Add SQL script for aggregations and window functions to analyze user bookings

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,31 @@
+-- Sql aggregations and window functions
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM 
+    User u
+LEFT JOIN 
+    Booking b ON u.user_id = b.user_id
+GROUP BY 
+    u.user_id, u.first_name, u.last_name
+ORDER BY 
+    total_bookings DESC;
+
+-- window function
+SELECT 
+    property_id,
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank
+FROM (
+    SELECT 
+        p.property_id,
+        COUNT(b.booking_id) AS total_bookings
+    FROM 
+        Property p
+    LEFT JOIN 
+        Booking b ON p.property_id = b.property_id
+    GROUP BY 
+        p.property_id
+) AS property_bookings;
