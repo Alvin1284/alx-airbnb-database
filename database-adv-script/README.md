9458724977659bb60a70998ced7f8d55de4acf12
Alvin Waweru <81688085+Alvin1284@users.noreply.github.com>
2025-05-16 23:00:37 +0300

    Add SQL scripts for demonstrating various types of joins

---
+++ database-adv-script/README.md
@@ -0,0 +1,10 @@
+# SQL Scripts demonstrating how to perform different Joins
+
+## Inner Join
+### This type of join returns records that have matching values in both tables.
+
+## Left Join
+### This type of join returns all records from the left table and the matched records from the right table
+
+## Outer Join
+### This type of join returns all records from both tables, with NULL in the columns where there are no matches.
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,64 @@
+-- This SQL script demonstrates how to perform various types of joins in SQL.
+-- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+SELECT 
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM 
+    Booking b
+INNER JOIN 
+    User u ON b.user_id = u.user_id;
+
+-- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.created_at AS review_created_at
+FROM 
+    Property p
+LEFT JOIN 
+    Review r ON p.property_id = r.property_id;
+
+-- 3. Outer JOIN
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price
+FROM 
+    User u
+LEFT JOIN 
+    Booking b ON u.user_id = b.user_id
+
+UNION
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price
+FROM 
+    Booking b
+LEFT JOIN 
+    User u ON b.user_id = u.user_id;
+
+
